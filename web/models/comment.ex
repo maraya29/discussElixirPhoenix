2ba3d200d46164d9a59d79425b15d824eec79ac6
maraya29 <mfasmachin@gmail.com>
2018-09-14 13:23:26 -0600

    added final updates for comments

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,10 +1,14 @@
 defmodule Discuss.Comment do
   use Discuss.Web, :model
 
+  # the only thing that poison needs to convert to json formant
+  # this has to be related to Discuss.CommentsChannel last line of join method
+  @derive {Poison.Encoder, only: [:content, :user]}
+
   schema "comments" do
     field(:content, :string)
-    belongs_to(:topic, Discuss.Topic)
     belongs_to(:user, Discuss.User)
+    belongs_to(:topic, Discuss.Topic)
 
     timestamps()
   end
